4b91f4da4ef1abe1a9352c5d4124abebbf6fdf32
Michał Meina <michal.meina@gmail.com>
2018-12-05 11:22:32 +0100

    load mzk sript

---
+++ load_mzk.py
@@ -0,0 +1,60 @@
+import neo4j
+
+f = open('mzk_data.txt')
+data = f.readlines()
+f.close()
+
+data = [l.strip() for l in data]
+sep_idx = data.index('=====')
+
+transp_data = {}
+
+while True:
+    line_data = data[:sep_idx]
+    data = data[sep_idx+1:]
+
+    bus_no = line_data[0]
+
+    split_idx = line_data.index('-----')
+    route = line_data[1:split_idx]
+    route = [l.split(' : ') for l in route]
+
+    time_table = line_data[split_idx+1:]
+    time_table = [l.split(' : ') for l in time_table]
+
+    if len(data) == 0: break
+
+    sep_idx = data.index('=====')
+
+    transp_data[bus_no] = (time_table, route)
+
+
+#%%
+
+stops = set()
+connected_stops = set()
+for bus_no, (time_table, route) in transp_data.iteritems():
+    for time, stop in route:
+        stops.add(stop)
+
+    for stop_from, stop_to in zip(route[1:], route[:-1]):
+        connected_stops.add((stop_from[1], stop_to[1]))
+
+
+
+def create_stops(tx, all_stops):
+    for stop in all_stops:
+        q = "CREATE (n:STOP {name:'%s'})" % stop
+        tx.run(q)
+
+def create_connections(tx, connected_stops):
+    for stop_from, stop_to in connected_stops:
+        q = ("MATCH (a:STOP{name:'%s'}),(b:STOP{name:'%s'})"  % (stop_from, stop_to)) + \
+                " CREATE (a)-[r:CONNECTED]->(b)"
+        tx.run(q)
+
+driver = neo4j.GraphDatabase().driver('bolt://localhost:7687', auth=('neo4j', 'dupablada'))
+
+with driver.session() as session:
+    session.write_transaction(create_stops, stops)
+    session.write_transaction(create_connections, connected_stops)
